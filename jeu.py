31fd145e05d37bbdeb3e508bfbbdee6b24340513
Terence Le Thierry <terence.lethierry@protonmail.com>
2025-02-05 16:04:32 +0100

    Ajouté la méthode executer() à jeu.py

--- jeu.py
+++ jeu.py
@@ -5,6 +5,32 @@ pygame.init() # Initialiser pygame
 
 class Jeu:
     """Classe représentant une partie de bataille navale."""
-    def __init__(self):
-        """Initilisation du jeu"""
-        pass
+    def __init__(self, titre:str, largeur:int, hauteur:int) -> None:
+        """Initilisation du jeu.
+        - titre: titre de la fenêtre de jeu
+        - largeur: largeur de la fenêtre de jeu
+        - hauteur: hauteur de la fenêtre de jeu"""
+
+        # Fenêtre du jeu
+        self.fenetre = pygame.display.set_mode((largeur, hauteur))
+        # Titre de la fenêtre
+        pygame.display.set_caption(titre)
+
+        # Reste du code du gameplay
+
+    def executer(self):
+        """Exécute la boucle de jeu."""
+        self.execution = True # Variable pour marquer l'état du jeu (en exécution ou non)
+
+        # Boucle de jeu
+        while self.execution:
+            # Détecter les événements de jeu et réagir en conséquence
+            for evenement in pygame.event.get():
+                # Si le joueur veut quitter le jeu
+                if evenement.type == pygame.QUIT:
+                    # Mettre la variable d'exécution à False 
+                    self.execution = False
+
+            # Mettre à jour l'affichage
+            pygame.display.flip()              
+        
--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 "main.py exécute le jeu"
 import pygame # Importer pygame
+pygame.init() # Initialiser pygame
+from jeu import * # Importer le contenu du fichier jeu.py
 
+
+# Exécuter une première partie
+jeu = Jeu(titre="Bataille navale !", largeur=800, hauteur=600)
+jeu.executer()
+
+# Demander au joueur s'il souhaite rejouer, si c'est le cas entrer dans une boucle
