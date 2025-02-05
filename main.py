cf2766d4f1094d599472ba2ca2be3237e6cf8642
Terence Le Thierry <terence.lethierry@protonmail.com>
2025-02-05 18:12:23 +0100

    Implémenté un boucle pour rejouer

--- main.py
+++ main.py
@@ -2,10 +2,22 @@
 import pygame # Importer pygame
 pygame.init() # Initialiser pygame
 from jeu import * # Importer le contenu du fichier jeu.py
+from tkinter import messagebox # Importer le module messagebox de tkinter
 
 
 # Exécuter une première partie
 jeu = Jeu(titre="Bataille navale !", largeur=800, hauteur=600)
 jeu.executer()
-
+del jeu
+pygame.quit()
 # Demander au joueur s'il souhaite rejouer, si c'est le cas entrer dans une boucle
+while True:
+    rejouer = messagebox.askyesno("Rejouer ?", "Voulez-vous rejouer ?") # Demander au joueur s'il souhaite rejouer et enregistrer la réponse
+    if rejouer: # Si le joueur veut rejouer
+        # Réinitialiser le jeu
+        pygame.init()
+        jeu = Jeu(titre="Bataille navale !", largeur=800, hauteur=600)
+        jeu.executer()
+
+    else:
+        break    
